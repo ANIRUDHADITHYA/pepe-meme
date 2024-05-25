a0f1086f424c502d21c0c7764f4a2e2ce5346912
ANIRUDHADITHYA <anirudhadithya.b.s@gmail.com>
2024-05-25 14:50:36 +0530

    updated meme

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="trump-meme-maker-container">
         <div className="navbar-container">
           <div className="logo-wrapper">
-            <img src="/Asserts/pepe/1.png"></img>
+            <img src="/Asserts/pepe/1.png" alt=" "></img>
           </div>
           <h1>PEPE GEN</h1>
         </div>
@@ -21,9 +21,9 @@ function App() {
           <div className="crete-meme-display-container">
             <div className="meme-mirror-container">
               <div className="meme-mirror-wrapper">
-                {background ? <img src={`/Asserts/background/${background}.png`} className='background'></img> : <img src={`/Asserts/background/1.png`}></img>}
-                {pepe ? <img src={`/Asserts/pepe/${pepe}.png`} className='pepe'></img> : ""}
-                {pant ? <img src={`/Asserts/pant/${pant}.png`} className='pant'></img> : ""}
+                {background ? <img src={`/Asserts/background/${background}.png`} className='background' alt=" "></img> : <img src={`/Asserts/background/1.png`} alt=" "></img>}
+                {pepe ? <img src={`/Asserts/pepe/${pepe}.png`} className='pepe' alt=" "></img> : ""}
+                {pant ? <img src={`/Asserts/pant/${pant}.png`} className='pant' alt=" "></img> : ""}
                 <div className='transperent'></div>
               </div>
             </div>
@@ -31,21 +31,21 @@ function App() {
               <h2>CREATE YOUR TRUMP</h2>
               <h3>Background</h3>
               <div className='trump--item-container background'>
-                <img src={'/Asserts/background/1.png'} onClick={() => { setBackground(1) }}></img>
-                <img src={'/Asserts/background/2.png'} onClick={() => { setBackground(2) }}></img>
-                <img src={'/Asserts/background/3.png'} onClick={() => { setBackground(3) }}></img>
+                <img src={'/Asserts/background/1.png'} onClick={() => { setBackground(1) }} alt=" "></img>
+                <img src={'/Asserts/background/2.png'} onClick={() => { setBackground(2) }} alt=" "></img>
+                <img src={'/Asserts/background/3.png'} onClick={() => { setBackground(3) }} alt=" "></img>
               </div>
               <h3>Pepe</h3>
               <div className='trump--item-container pepe'>
-                <img src={'/Asserts/pepe/1.png'} onClick={() => { setPepe(1) }}></img>
-                <img src={'/Asserts/pepe/2.png'} onClick={() => { setPepe(2) }}></img>
-                <img src={'/Asserts/pepe/3.png'} onClick={() => { setPepe(3) }}></img>
+                <img src={'/Asserts/pepe/1.png'} onClick={() => { setPepe(1) }} alt=" "></img>
+                <img src={'/Asserts/pepe/2.png'} onClick={() => { setPepe(2) }} alt=" "></img>
+                <img src={'/Asserts/pepe/3.png'} onClick={() => { setPepe(3) }} alt=" "></img>
               </div>
               <h3>Pant</h3>
               <div className='trump--item-container pant'>
-                <img src={'/Asserts/pant/1.png'} onClick={() => { setPant(1) }}></img>
-                <img src={'/Asserts/pant/2.png'} onClick={() => { setPant(2) }}></img>
-                <img src={'/Asserts/pant/3.png'} onClick={() => { setPant(3) }}></img>
+                <img src={'/Asserts/pant/1.png'} onClick={() => { setPant(1) }} alt=" "></img>
+                <img src={'/Asserts/pant/2.png'} onClick={() => { setPant(2) }} alt=" "></img>
+                <img src={'/Asserts/pant/3.png'} onClick={() => { setPant(3) }} alt=" "></img>
               </div>
             </div>
           </div>
